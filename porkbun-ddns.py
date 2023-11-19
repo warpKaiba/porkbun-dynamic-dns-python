878327bc27982fd05efc94fe712d86d685c9b1b9
kaiba <27407917+warpKaiba@users.noreply.github.com>
2023-11-19 17:25:48 -0600

    Update porkbun-ddns.py
    Restructure script to use edit api endpoint instead of delete+create, and only update if something has changed

--- porkbun-ddns.py
+++ porkbun-ddns.py
@@ -3,30 +3,31 @@ import requests
 import re
 import sys
 
-def getRecords(domain): #grab all the records so we know which ones to delete to make room for our record. Also checks to make sure we've got the right domain
+def getRecord(domain, subdomain=""): #grabs the record for the specified sub/domain
 	allRecords=json.loads(requests.post(apiConfig["endpoint"] + '/dns/retrieve/' + domain, data = json.dumps(apiConfig)).text)
 	if allRecords["status"]=="ERROR":
 		print('Error getting domain. Check to make sure you specified the correct domain, and that API access has been switched on for this domain.');
 		sys.exit();
-	return(allRecords)
-	
+	for i in allRecords["records"]:
+		if i["name"]==fqdn and (i["type"] == 'A' or i["type"] == 'ALIAS' or i["type"] == 'CNAME'):
+			record = i
+			return(record)
+
 def getMyIP():
 	ping = json.loads(requests.post(apiConfig["endpoint"] + '/ping/', data = json.dumps(apiConfig)).text)
 	return(ping["yourIp"])
 
-def deleteRecord():
-	for i in getRecords(rootDomain)["records"]:
-		if i["name"]==fqdn and (i["type"] == 'A' or i["type"] == 'ALIAS' or i["type"] == 'CNAME'):
-			print("Deleting existing " + i["type"] + " Record")
-			deleteRecord = json.loads(requests.post(apiConfig["endpoint"] + '/dns/delete/' + rootDomain + '/' + i["id"], data = json.dumps(apiConfig)).text)
-
-def createRecord():
-	createObj=apiConfig.copy()
-	createObj.update({'name': subDomain, 'type': 'A', 'content': myIP, 'ttl': 300})
-	endpoint = apiConfig["endpoint"] + '/dns/create/' + rootDomain
-	print("Creating record: " + fqdn + " with answer of " + myIP)
-	create = json.loads(requests.post(apiConfig["endpoint"] + '/dns/create/'+ rootDomain, data = json.dumps(createObj)).text)
-	return(create)
+def editRecord():
+	i = getRecord(rootDomain, subDomain)
+	print("Checking", i['name'])
+	if i["content"] == myIP:
+		print("IP is the same, not changing anything")
+	else:
+		print("Editing existing " + i["type"] + " Record with " + myIP)
+		createObj=apiConfig.copy()
+		createObj.update({'content': myIP, 'ttl': i['ttl'], 'type': i["type"]})
+		editRecord = json.loads(requests.post(apiConfig["endpoint"] + '/dns/edit/' + rootDomain + "/" + i["id"], data = json.dumps(createObj)).text)
+		print(editRecord['status'])
 
 if len(sys.argv)>2: #at least the config and root domain is specified
 	apiConfig = json.load(open(sys.argv[1])) #load the config file into a variable
@@ -46,8 +47,7 @@ if len(sys.argv)>2: #at least the config and root domain is specified
 	else:
 		myIP=getMyIP() #otherwise use the detected exterior IP address
 	
-	deleteRecord()
-	print(createRecord()["status"])
+	editRecord()
 	
 else:
 	print("Porkbun Dynamic DNS client, Python Edition\n\nError: not enough arguments. Examples:\npython porkbun-ddns.py /path/to/config.json example.com\npython porkbun-ddns.py /path/to/config.json example.com www\npython porkbun-ddns.py /path/to/config.json example.com '*'\npython porkbun-ddns.py /path/to/config.json example.com -i 10.0.0.1\n")
